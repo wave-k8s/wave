3f8de77dd5c91bf870b0d632066d5f466e0dc52f
Joel Speed <Joel.speed@hotmail.co.uk>
2019-05-03 11:22:45 +0100

    Remove redudent Update Matcher

--- test/utils/matchers.go
+++ test/utils/matchers.go
@@ -57,14 +57,6 @@ func (m *Matcher) Delete(obj Object, extras ...interface{}) gomega.GomegaAsserti
 	return gomega.Expect(err, extras)
 }
 
-// Update udpates the object on the API server
-func (m *Matcher) Update(obj Object, intervals ...interface{}) gomega.GomegaAsyncAssertion {
-	update := func() error {
-		return m.Client.Update(context.TODO(), obj)
-	}
-	return gomega.Eventually(update, intervals...)
-}
-
 // UpdateWithFunc udpates the object on the API server by fetching the object
 // and applying a mutating UpdateFunc before sending the update
 func (m *Matcher) UpdateWithFunc(obj Object, fn UpdateFunc, intervals ...interface{}) gomega.GomegaAsyncAssertion {
